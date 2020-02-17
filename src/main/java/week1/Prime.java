16754444464964a3dac25a19aa0d14d7061bfdcd
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-02-17 22:50:57 +0100

    add week 1 first assignment

--- src/main/java/week1/Prime.java
+++ src/main/java/week1/Prime.java
@@ -1,5 +1,25 @@
 package week1;
 
-public class Prime {
+class Prime {
 
+    /**
+     * Checks whether the given integer value is a prime number.
+     *
+     * @param n integer value to be checked if it is a prime number or not
+     * @return returns true if n is prime, false otherwise
+     */
+    public static boolean isPrime(int n) {
+        // TODO
+    }
+
+    /**
+     * Counts and returns the number of prime numbers that are less or equal
+     * than the given integer value.
+     *
+     * @param n integer value defining an upper bound on the set of prime number to count
+     * @return returns the number of prime numbers that are less or equal than n
+     */
+    public static int numPrimes(int n) {
+        // TODO
+    }
 }

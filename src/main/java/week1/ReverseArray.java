55e840da5816166f5857d0a3799a14ef742f94dd
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-02-20 17:22:59 +0100

    finish week 1 reverse Array

--- src/main/java/week1/ReverseArray.java
+++ src/main/java/week1/ReverseArray.java
@@ -4,10 +4,17 @@ public class ReverseArray {
     /**
      * Reverses the order of the elements of the given array.
      *
-     * @param arr
-     * the array to reverse
+     * @param arr the array to reverse
      */
     public static void reverse(int[] arr) {
-        // TODO
+        if (arr == null) {
+            return;
+        }
+        // reverse an array in-place
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - (1 + i)];
+            arr[arr.length - (1 + i)] = temp;
+        }
     }
 }
--- src/test/java/week1/ReverseArrayTest.java
+++ src/test/java/week1/ReverseArrayTest.java
@@ -6,11 +6,19 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class ReverseArrayTest {
 
+
+    @Test
+    public void nullTest() {
+        int[] arr = null;
+        ReverseArray.reverse(arr);
+        assertArrayEquals(null, arr);
+    }
+
     @Test
     public void testExample() {
-        int[] arr = { 1, 2, 3, 4, 5 };
+        int[] arr = {1, 2, 3, 4, 5, 6};
         ReverseArray.reverse(arr);
-        int[] result = { 5, 4, 3, 2, 1 };
+        int[] result = {6, 5, 4, 3, 2, 1};
         assertArrayEquals(result, arr);
     }
 }

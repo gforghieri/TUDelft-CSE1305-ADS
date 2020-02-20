a00dbf17541e6aca6de13173189fc9f63c66ae5c
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-02-20 23:14:10 +0100

    add tests for exceptional MergeSortedArray cases

--- src/test/java/week1/MergeSortedArrayTest.java
+++ src/test/java/week1/MergeSortedArrayTest.java
@@ -13,5 +13,34 @@ public class MergeSortedArrayTest {
         int[] arr = MergeSortedArray.merge(arr1, arr2);
         assertArrayEquals(new int[]{1, 2, 2, 3, 4, 5, 6}, arr);
     }
+
+    // Exceptional cases.
+
+    // if both arrays are null, the result should be null as well.
+    @Test
+    public void testBothNull() {
+        int[] arr1 = null;
+        int[] arr2 = null;
+        int[] arr = MergeSortedArray.merge(arr1, arr2);
+        assertArrayEquals(null, arr);
+    }
+
+    @Test
+    public void testFirstNull() {
+        int[] arr1 = null;
+        int[] arr2 = {2, 3, 5, 6};
+        int[] arr3 = {2, 3, 5, 6};
+        int[] arr = MergeSortedArray.merge(arr1, arr2);
+        assertArrayEquals(arr3, arr);
+    }
+
+    @Test
+    public void testSecondNull() {
+        int[] arr1 = {2, 3, 5, 6};
+        int[] arr2 = null;
+        int[] arr3 = {2, 3, 5, 6};
+        int[] arr = MergeSortedArray.merge(arr1, arr2);
+        assertArrayEquals(arr3, arr);
+    }
 }
 

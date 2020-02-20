575e393018ec5232c94ccda6340b65bb32d99355
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-02-20 22:57:16 +0100

    add first solution for merge sorted arrays 50/100

--- src/main/java/week1/MergeSortedArray.java
+++ src/main/java/week1/MergeSortedArray.java
@@ -1,5 +1,6 @@
 package week1;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 class MergeSortedArray {
@@ -19,6 +20,54 @@ class MergeSortedArray {
      * @return sorted array containing all elements from both arrays
      */
     public static int[] merge(int[] arr1, int[] arr2) {
-        // TODO
+
+        // Exceptional cases.
+
+        // if both arrays are null, the result should be null as well.
+        if (arr1 == null && arr2 == null) {
+            return null;
+        }
+
+        // If either one of the arrays are null, return the non-null array as a copy
+        else if (arr2 == null) {
+            int[] arrCopy = new int[arr1.length];
+            for (int i = 0; i < arr1.length; i++) {
+                arrCopy[i] = arr1[i];
+            }
+            return arrCopy;
+        }
+        // If either one of the arrays are null, return the non-null array as a copy
+        else if (arr1 == null) {
+            int[] arrCopy = new int[arr2.length];
+            for (int i = 0; i < arr2.length; i++) {
+                arrCopy[i] = arr2[i];
+            }
+            return arrCopy;
+        }
+
+        // Normal case
+        int[] mergedArray = new int[arr1.length + arr2.length];
+
+        int newPositionCounter = 0;
+        int oldPositionCounter = 0;
+
+        for (int i = 0; i < Math.min(arr1.length, arr2.length); i++) {
+            oldPositionCounter = i;
+            mergedArray[newPositionCounter] = Math.min(arr1[i], arr2[i]);
+            newPositionCounter++;
+            mergedArray[newPositionCounter] = Math.max(arr1[i], arr2[i]);
+            newPositionCounter++;
+        }
+        oldPositionCounter++;
+
+        for (int i = oldPositionCounter; i < newPositionCounter; i++) {
+            if (Math.max(arr1.length, arr2.length) == arr1.length) {
+                mergedArray[newPositionCounter] = arr1[oldPositionCounter];
+            } else {
+                mergedArray[newPositionCounter] = arr2[oldPositionCounter];
+            }
+        }
+        return mergedArray;
     }
 }
+

4d4a61d79ae0cbde2f9148fa48826276d4500d3d
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-02-25 18:42:40 +0100

    week 1 CountRepetitions 83/100

--- src/main/java/week1/CountRepetitions.java
+++ src/main/java/week1/CountRepetitions.java
@@ -1,6 +1,5 @@
 package week1;
 
-
 public class CountRepetitions {
 
     /**
@@ -12,13 +11,20 @@ public class CountRepetitions {
      * If the input array is null or of length 0, this will return null.
      *
      * @param arr the array of integer elements to be counted.
-     * @param r the integer indicating the last element of the range.
+     * @param r   the integer indicating the last element of the range.
      * @return a new array containing the number of occurrences of each
      * integer {0, ..., r} in the input array (index i has the
      * count of elements equal to i in the input array, with i
      * in {0, ..., r})
      */
     public static int[] count(int[] arr, int r) {
-        // TODO
+        // Exceptional cases
+        // 1. If the input array is null or of length 0, this will return null.
+        if (arr == null || arr.length == 0) return null;
+
+        // Normal case
+        int[] result = new int[r + 1];
+        for (int x : arr) if (x >= 0 && x < r) result[x]++;
+        return result;
     }
 }
--- src/test/java/week1/CountRepetitionsTest.java
+++ src/test/java/week1/CountRepetitionsTest.java
@@ -1,14 +1,50 @@
 package week1;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
-import org.junit.*;
+import static org.junit.Assert.assertArrayEquals;
 
 public class CountRepetitionsTest {
 
-    public void testExample() {
+    @Test
+    public void testOrderedPositive() {
         int[] arr = {0, 1, 1, 3, 3, 3, 8, 10};
         int[] result = {1, 2, 0, 3, 0};
+        assertArrayEquals(result, CountRepetitions.count(arr, 4));
+    }
+
+    @Test
+    public void testNotOrderedPositive() {
+        int[] arr = {0, 8, 1, 3, 1, 3, 10, 3};
+        int[] result = {1, 2, 0, 3, 0};
+        assertArrayEquals(result, CountRepetitions.count(arr, 4));
+    }
+
+    @Test
+    public void testExampleNull() {
+        int[] arr = null;
+        int[] result = null;
+        assertArrayEquals(result, CountRepetitions.count(arr, -3));
+    }
+
+    @Test
+    public void testExampleLength0() {
+        int[] arr = {};
+        int[] result = null;
+        assertArrayEquals(result, CountRepetitions.count(arr, 4));
+    }
+
+    @Test
+    public void testNegativeOrdered() {
+        int[] arr = {-2, -1, 0, 1, 1, 3, 3, 10};
+        int[] result = {1, 2, 0, 2, 0};
+        assertArrayEquals(result, CountRepetitions.count(arr, 4));
+    }
+
+    @Test
+    public void testNegativeNotOrdered() {
+        int[] arr = {-2, -1, 0, 1, 1, 3, 3, 10};
+        int[] result = {1, 2, 0, 2, 0};
         assertArrayEquals(result, CountRepetitions.count(arr, 4));
     }
 }

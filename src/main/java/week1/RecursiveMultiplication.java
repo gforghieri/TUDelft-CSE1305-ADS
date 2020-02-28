3debce62fe2fb67525beb996cf9afb213f5cb79f
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-02-28 19:42:18 +0100

    add week 1 RecursiveMultiplication 100/100 and 7 tests

--- src/main/java/week1/RecursiveMultiplication.java
+++ src/main/java/week1/RecursiveMultiplication.java
@@ -1,7 +1,37 @@
 package week1;
 
+/**
+ * Be recursive, that is, calculate the result using recursion; Not use the multiplication operator.
+ * Instead, you can make us of the addition +, subtraction -, equality ==, and inequality < operators.
+ * IMPORTANT: In an exam setting, your grade for this exercise would be overridden to 1,
+ * if you don’t satisfy the conditions mentioned above.
+ */
 public class RecursiveMultiplication {
     static int multiply(int num1, int num2) {
-        // TODO
+
+
+        // num 1 = 0 && num 2 = 0 - - DONE
+
+        // num 1= 0 && num 2 = positive - DONE
+        // num 1 = 0 && num 2 = negative - - DONE
+
+        // num 1 = positive && num 2 = 0 - DONE
+        // num 1 = negative && num 2 = 0 - - DONE
+        if (num1 == 0 || num2 == 0) {
+            return 0;
+        }
+
+        int res = num1;
+        // add  num1 to itself as many times as num2
+
+        // num 1 = positive, num 2 = negative - DONE
+        // num 1 = negative, num 2 = negative - DONE
+        if (num2 < 0) {
+            return -res + multiply(num1, num2 + 1);
+        }
+
+        // num 1 = positive, num 2 = positive - DONE
+        // num 1 = negative, num 2 = positive - DONE
+        return res + (multiply(num1, num2 - 1));
     }
 }
--- src/test/java/week1/RecursiveMultiplicationTest.java
+++ src/test/java/week1/RecursiveMultiplicationTest.java
@@ -4,9 +4,41 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 
-public class UTest {
+public class RecursiveMultiplicationTest {
     @Test
     public void mp() {
-        assertEquals(20, RecursiveMultiplication.multiply(4, 5));
+        assertEquals(30, Solution.multiply(6, 5));
     }
+
+    @Test
+    public void both0() {
+        assertEquals(0, Solution.multiply(0, 0));
+    }
+
+    @Test
+    public void one0otherPos() {
+        assertEquals(0, Solution.multiply(0, 2));
+    }
+
+    @Test
+    public void one0otherNeg() {
+        assertEquals(0, Solution.multiply(-5, 0));
+    }
+
+    @Test
+    public void onePostotherNeg() {
+        assertEquals(-5, Solution.multiply(-5, 1));
+    }
+
+    @Test
+    public void firstPostOtherNeg() {
+        assertEquals(-15, Solution.multiply(5, -3));
+    }
+
+    @Test
+    public void bothNeg() {
+        assertEquals(15, Solution.multiply(-5, -3));
+    }
+
+
 }

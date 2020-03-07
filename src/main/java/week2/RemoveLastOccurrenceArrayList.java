ebf4ca7f0b84e83d14d6f9c0a555da673850c08e
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-03-07 21:45:32 +0100

    add week2 RemoveLastOccurrence and RemoveAll ArrayList

--- src/main/java/week2/RemoveLastOccurrenceArrayList.java
+++ src/main/java/week2/RemoveLastOccurrenceArrayList.java
@@ -6,11 +6,19 @@ class RemoveLastOccurrenceArrayList {
     /**
      * Removes all elements from the ArrayList, using the removeLastOccurrence method.
      *
-     * @param list
-     *     to remove the elements from.
+     * @param list to remove the elements from.
      */
     public static void removeAll(ArrayList<Integer> list) {
-        // TODO
+        // if the ArrayList is empty
+        if (list == null || list.size() == 0) {
+            return;
+        }
+        int originalSize = list.size();
+
+        // loop through the array and target the 0th element in the ArrayList to remove the last occurrence of
+        for (int i = 0; i < originalSize ; i++) {
+            removeLastOccurrence(list.get(0), list);
+        }
     }
 
     /**
@@ -21,12 +29,29 @@ class RemoveLastOccurrenceArrayList {
      * Note that this method does not return a new list.
      * Instead, the list that is passed as a parameter is changed.
      *
-     * @param list
-     *     to remove an element from.
-     * @param x
-     *     element value to look for
+     * @param list to remove an element from.
+     * @param x    element value to look for
      */
     public static void removeLastOccurrence(int x, ArrayList<Integer> list) {
-        // TODO
+
+        // if the ArrayList is empty
+        if (list == null || list.size() == 0) {
+            return;
+        }
+
+        int lastIndex = -1;
+
+        // loop through the ArrayList find last_index of x
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == x) {
+                lastIndex = i;
+            }
+        }
+
+        // if x was not part of the ArrayList return
+        if (lastIndex == -1) return;
+
+        // remove list[last_index]
+        list.remove(lastIndex);
     }
 }
--- src/test/java/week2/RemoveLastOccurenceArrayListTest.java
+++ src/test/java/week2/RemoveLastOccurenceArrayListTest.java
@@ -1,6 +1,7 @@
 package week2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import static java.util.Arrays.*;
 import static org.junit.Assert.*;
@@ -14,6 +15,14 @@ public class RemoveLastOccurenceArrayListTest {
         ArrayList<Integer> input = new ArrayList<>(asList(1, 1, 3, 5, 7, 1, 5, 9, 1));
         ArrayList<Integer> result = new ArrayList<>(asList(1, 1, 3, 5, 7, 1, 5, 9));
         RemoveLastOccurrenceArrayList.removeLastOccurrence(1, input);
+        assertEquals(result, input);
+    }
+
+    @Test
+    public void testRemoveAll() {
+        ArrayList<Integer> input = new ArrayList<Integer>(asList(1, 3, 4));
+        ArrayList<Integer> result = new ArrayList<>(Collections.emptyList());
+        RemoveLastOccurrenceArrayList.removeAll(input);
         assertEquals(result, input);
     }
 }

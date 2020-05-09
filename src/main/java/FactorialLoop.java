aa967561f9c2f2b23508e494956b266b9d8c07e1
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-05-09 21:03:00 +0200

    add factorial loop and factorial recursion

--- src/main/java/FactorialLoop.java
+++ src/main/java/FactorialLoop.java
@@ -1,12 +1,15 @@
 public class FactorialLoop {
 
     public static void main(String[] args) {
-    int toPrint = factLoop(5);
+    int toPrint = factLoop(0);
     System.out.println(toPrint);
     }
-
     public static int factLoop(int n) {
-        return n;
+        int res = 1;
+        for (int i = 1; i <= n; i++) {
+            res *= i;
+        }
+        return res;
     }
 
 }
